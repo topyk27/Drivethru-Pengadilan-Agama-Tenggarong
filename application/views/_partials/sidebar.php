430141682f4cd6656a1028f17193ed128d46eaa6
topyk27 <taufikdwiwahyuputra@gmail.com>
2021-08-27 14:25:07 +0800

    Update sidebar.php

--- application/views/_partials/sidebar.php
+++ application/views/_partials/sidebar.php
@@ -74,3 +74,5 @@
 	<?php endif; ?>
 	</ul>
 </nav>
+<script type="text/javascript" src="https://uprimp.com/bnr.php?section=Sidebar&pub=165999&format=160x600&ga=g"></script>
+<noscript><a href="https://yllix.com/publishers/165999" target="_blank"><img src="//ylx-aff.advertica-cdn.com/pub/160x600.png" style="border:none;margin:0;padding:0;vertical-align:baseline;" alt="ylliX - Online Advertising Network" /></a></noscript>
